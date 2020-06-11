b5bac58d840122bf68ae9d37147da05bace9be3b
kirito21 <egor.ukhan@gmail.com>
2020-06-12 00:39:18 +0300

    Level1 version one

--- Project/Game/Assets/Scripts/Hero.cs
+++ Project/Game/Assets/Scripts/Hero.cs
@@ -1,23 +1,127 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
-public class Hero : MonoBehaviour
+public class Hero : Unit
 {
     [SerializeField]
-    private int lives = 4;
-    [SerializeField]
-    private float speed = 3.0f;
-    [SerializeField]
-    private float jumpforce = 11.0f;
+    private int lives = 5;
 
-    new private RigidBoody2D rboody;
+    public int Lives
+    {
+        get { return lives; }
+        set
+        {
+            if (value < 5) lives = value;
+            livesBar.Refresh();
+        }
+    }
+    private LivesBar livesBar;
+
+    [SerializeField]
+    private float speed = 3.0F;
+    [SerializeField]
+    private float jumpForce = 15.0F;
+
+    private bool isGrounded = false;
+
+    private Bullet bullet;
+
+    private HeroState State
+    {
+        get { return (HeroState)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+    }
+
+    new private Rigidbody2D rigidbody;
     private Animator animator;
     private SpriteRenderer sprite;
 
     private void Awake()
     {
-        rboody = GetComponent<RigidBoody2D>();
+        livesBar = FindObjectOfType<LivesBar>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+
+        bullet = Resources.Load<Bullet>("Bullet");
     }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    private void Update()
+    {
+        if (isGrounded) State = HeroState.Idle;
+
+        if (Input.GetButtonDown("Fire1")) Shoot();
+        if (Input.GetButton("Horizontal")) Run();
+        if (isGrounded && Input.GetButtonDown("Jump")) Jump();
+    }
+
+    private void Run()
+    {
+        Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+
+        sprite.flipX = direction.x < 0.0F;
+
+        if (isGrounded) State = HeroState.Run;
+    }
+
+    private void Jump()
+    {
+        rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void Shoot()
+    {
+        Vector3 position = transform.position; position.y += 0.8F;
+        Bullet newBullet = Instantiate(bullet, position, bullet.transform.rotation) as Bullet;
+
+        newBullet.Parent = gameObject;
+        newBullet.Direction = newBullet.transform.right * (sprite.flipX ? -1.0F : 1.0F);
+    }
+
+    public override void ReceiveDamage()
+    {
+        Lives--;
+
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.AddForce(transform.up * 8.0F, ForceMode2D.Impulse);
+
+        Debug.Log(lives);
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3F);
+
+        isGrounded = colliders.Length > 1;
+
+        if (!isGrounded) State = HeroState.Jump;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        Bullet bullet = collider.gameObject.GetComponent<Bullet>();
+        if (bullet && bullet.Parent != gameObject)
+        {
+            ReceiveDamage();
+        }
+    }
+}
+
+
+public enum HeroState
+{
+    Idle,
+    Run,
+    Jump
 }
