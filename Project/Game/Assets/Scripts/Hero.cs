23af235d6c744af7836bcdfd45006a34bb3254e6
kirito21 <egor.ukhan@gmail.com>
2020-06-11 21:39:38 +0300

    start

---
+++ Project/Game/Assets/Scripts/Hero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField]
+    private int lives = 4;
+    [SerializeField]
+    private float speed = 3.0f;
+    [SerializeField]
+    private float jumpforce = 11.0f;
+
+    new private RigidBoody2D rboody;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rboody = GetComponent<RigidBoody2D>();
+    }
+}
